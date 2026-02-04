e68595bf3b8883a6447038b3e3de3423d476d973
Vishal Shukla <vishalshukla1029@gmail.com>
2026-02-04 10:17:48 +0530

    new changes

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ ssh -i ~/.ssh/id_rsa -f -N -A -J vishal@13.127.240.17 vishal@10.0.0.82 -L 27017:
 
 echo "✅ Tunnel started."
 echo "🚀 Connecting to Remote MongoDB..."
-echo "🚀 Connected to Remote MongoDB."
+echo "🎉 Connected to Remote MongoDB."
 
 #mongosh "mongodb://127.0.0.1:27017/zoppertrack?directConnection=true"
 
